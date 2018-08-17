ea650a84ac15ca1ae07cb0746c32e4aceaa91467
BIT student <classroom@bit.rs>
2018-08-17 15:58:24 +0200

    ...

--- bit-blog/js/data/constants/constants.js
+++
@@ -1,4 +0,0 @@
-export const postsEndpoint = "https://jsonplaceholder.typicode.com/posts";
-export const singlePostEndpoint = "https://jsonplaceholder.typicode.com/posts/";
-export const authorsEndpoint = "https://jsonplaceholder.typicode.com/users";
-export const singleAuthorEndpoint = "https://jsonplaceholder.typicode.com/users?userId=1";
--- bit-blog/js/data/entities/post.js
+++
@@ -1,8 +0,0 @@
-export class Post {
-    constructor(userId, id, title, body) {
-        this.userId = userId;
-        this.id = id;
-        this.title = title;
-        this.body = body;
-    }
-}
